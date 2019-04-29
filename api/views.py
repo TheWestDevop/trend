67904b45db968c6ad069775c1c129d1101783bbf
thewestdevop <maxistinlove@gmail.com>
2019-04-29 13:15:59 +0100

    api update

--- api/views.py
+++ api/views.py
@@ -6,19 +6,9 @@ from django.views.decorators.http import *
 #create your view
 @require_http_methods("GET")
 def getAllArrticles(request):
-        article =  Articles.objects.all()
-        data = {
-            'id':article.id,
-            'title':article.title,
-            'summary':article.summary,
-            'shortdesc':article.shortdesc,
-            'content':article.content,
-            'sid':article.sid,
-            'status':article.status,
-            'author':article.author,
-            'pubdate':article.pubdate
-        }
-        return JsonResponse(data)
+        article = Articles.objects.all()
+       
+        return JsonResponse(article)
 
             
 
