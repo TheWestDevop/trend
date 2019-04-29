dcc2b76f1a945563e42c64d47cbe104c5f149113
thewestdevop <maxistinlove@gmail.com>
2019-04-29 13:06:07 +0100

    api update

--- api/views.py
+++ api/views.py
@@ -3,16 +3,6 @@ from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.views.decorators.http import *
 
-
-       
-       summary = models.TextField()
-       shortdesc = models.TextField()
-       content = models.TextField()
-       sid  = models.IntegerField()
-       status = models.IntegerField()
-       author = models.CharField(max_length=200)
-       pubdate =  models.CharField(max_length=255)
-
 #create your view
 @require_GET()
 def getAllArrticles(request):
